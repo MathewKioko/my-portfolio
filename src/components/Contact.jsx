3c12795bddc42457a94626212c2a0aec3d2ba679
MathewKioko <mathewkioko2006@gmail.com>
2026-01-15 06:50:17 +0300

    Remove success parameter from redirect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -181,7 +181,7 @@ const Contact = () => {
               {/* FormSubmit Configuration Fields */}
               <input type="hidden" name="_replyto" value={formData.email} />
               <input type="hidden" name="_subject" value="New Contact Form Submission from Portfolio" />
-              <input type="hidden" name="_next" value="/contact?success=true" />
+              <input type="hidden" name="_next" value="/contact" />
               <input type="hidden" name="_captcha" value="false" />
               <input type="hidden" name="_template" value="table" />
               <input type="text" name="_honey" style={{display: 'none'}} />
